196803bef2c447709737b0b6cdca3dd46700b16a
Atik Faysal <atikfaysal.jatri@gmail.com>
2024-11-11 17:39:53 +0600

    login screen

--- lib/main.dart
+++ lib/main.dart
@@ -13,69 +13,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: PasswordField(),
-    );
-  }
-}
-
-class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(56.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            // Profile Picture
-            const CircleAvatar(
-              radius: 50,
-              backgroundImage: AssetImage('assets/images/profile.jpg'),
-            ),
-            const SizedBox(height: 16),
-
-            // User Name
-            const Text(
-              'John Doe',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 8),
-
-            // User Email
-            const Text(
-              'johndoe@example.com',
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.grey,
-              ),
-            ),
-            const SizedBox(height: 16),
-
-            // Edit Profile Button
-            ElevatedButton(
-              onPressed: () {
-                // Handle button press
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(
-                    content: Text('Edit Profile button pressed!'),
-                  ),
-                );
-              },
-              child: const Text('Edit Profile'),
-            ),
-          ],
-        ),
-      ),
+      home: const PasswordField(),
     );
   }
 }
