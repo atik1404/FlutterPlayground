056c34f0f74e5b45c7d210af46e04c79ea50050e
Atik Faysal <atikfaysal.jatri@gmail.com>
2025-04-15 18:05:59 +0600

    chore(bloc): working on bloc pattern

---
+++ lib/feature/playground/bloc_playground_screen.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class BlocPlaygroundScreen extends StatelessWidget {
+  const BlocPlaygroundScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("Hello world");
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,10 @@
+import 'package:first_flutter/feature/playground/bloc_playground_screen.dart';
 import 'package:first_flutter/navigation/app_navigation.dart';
 import 'package:flutter/material.dart';
 
-import 'feature/notification/notification_repository.dart';
-import 'feature/notification/notification_screen.dart';
-
-// void callbackDispatcher() {
-//   Workmanager().executeTask((task, inputData) async {
-//     Logger.log("[WorkManager] Task executed at ${DateTime.now()}");
-//     return Future.value(true);
-//   });
-// }
-//
-// void startRecursiveWorkManager() async {
-//   while (true) {
-//     await Future.delayed(const Duration(seconds: 5));
-//
-//     Workmanager().registerOneOffTask(
-//       "recurringTask_${DateTime.now().millisecondsSinceEpoch}", // Unique task name
-//       "fetchData",
-//     );
-//
-//     Logger.log("[WorkManager] Scheduled a new task at ${DateTime.now()}");
-//     LocationService.locationStream.listen((position) {
-//       Logger.log("Updated location: ${position?.latitude}, ${position?.longitude}");
-//     });
-//   }
-// }
-
-
 void main() async{
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await LocationService.getCurrentLocation();
-  // Workmanager().initialize(callbackDispatcher, isInDebugMode: true);
-  // startRecursiveWorkManager();
-  // await LocationService.getCurrentLocation();
-  WidgetsFlutterBinding.ensureInitialized();
-  await NotificationRepository.initialization();
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -51,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const NotificationScreen(),
+      home: const BlocPlaygroundScreen(),
       onGenerateRoute: appNavigation.onNavigationChange,
       debugShowCheckedModeBanner: false,
     );
